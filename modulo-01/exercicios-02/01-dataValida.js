ca9514a423f105984de2d19bc237579d0cbb7eeb
TI Artecola <jonas.schlemmer@gmail.com>
2025-06-09 12:44:00 -0300

    Renamed files

---
+++ modulo-01/exercicios-02/01-dataValida.js
@@ -0,0 +1,1 @@
+// 1. Crie a função ehDataValida(dia, mes, ano) que retorne true se os valores formarem  uma  data  real  (meses  de  28–31  dias,  ano  bissexto  para fevereiro) e false caso contrário.
