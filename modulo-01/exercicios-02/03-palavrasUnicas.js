7fd8175f31cc9bb4d61983a79e036881c22eeae7
jonas-schlemmer <jonas.schlemmer@gmail.com>
2025-06-23 20:05:07 -0300

    3rd exercise completed

--- modulo-01/exercicios-02/03-palavrasUnicas.js
+++ modulo-01/exercicios-02/03-palavrasUnicas.js
@@ -1,1 +1,25 @@
 // 3. Dada uma string (ex.: "olá olá mundo mundo"), use if/else e for para extrair todas as palavras únicas e exibi-las em um array
+const prompt = require('prompt-sync')()
+
+const text = prompt("Digite uma frase: ")
+const words = text.split(" ")
+const unique = []
+
+for (let i = 0; i < words.length; i++) {
+    let word = words[i].toLowerCase()
+    let found = false
+
+    // Verifica se a palavra já está no array de únicas
+    for (let j = 0; j < unique.length; j++) {
+        if (word === unique[j]) {
+        found = true
+        break
+    }
+  }
+
+    if (!found) {
+        unique.push(word)
+    }
+}
+
+console.log("Palavras únicas:", unique)
