fce979c5600a282c2149562a258fd14f99d64dba
TI Artecola <jonas.schlemmer@gmail.com>
2025-06-24 10:34:25 -0300

    4th exercise completed

--- modulo-01/exercicios-02/04-fatorialRecursivo.js
+++ modulo-01/exercicios-02/04-fatorialRecursivo.js
@@ -1,1 +1,18 @@
 // 4. Implemente function fatorial(n) de forma recursiva; trate n < 0 lançando um Error, e n === 0 retornando 1
+function fatorial(n) {
+    if (n < 0) {
+        throw new Error("O fatorial não está definido para números negativos.")
+    }
+  
+    if (n === 0) {
+        return 1
+    }
+
+    return n * fatorial(n - 1)
+}
+
+console.log(fatorial(5)); // 120
+console.log(fatorial(0)); // 1
+console.log(fatorial(1)); // 1
+console.log(fatorial(3)); // 6
+console.log(fatorial(-2)); // Lança um erro
