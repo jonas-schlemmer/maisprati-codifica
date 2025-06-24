905d2551a9be408846e43b769e9715ef18d25bb0
Jonas Schlemmer <jonas.schlemmer@gmail.com>
2025-06-24 10:39:19 -0300

    5th exercise completed

--- modulo-01/exercicios-02/05-debounce.js
+++ modulo-01/exercicios-02/05-debounce.js
@@ -1,1 +1,14 @@
 // 5. Crie function debounce(fn, delay) que receba uma função fn e um delay em ms, retornando uma nova função que só executa fn se não for chamada novamente dentro do intervalo.
+function debounce(fn, delay) {
+    let timeoutId
+
+    return function(...args) {
+        // Limpa o timeout anterior, se houver
+        clearTimeout(timeoutId)
+
+        // Define um novo timeout para executar fn depois do delay
+        timeoutId = setTimeout(() => {
+            fn.apply(this, args)
+        }, delay)
+    }
+}
