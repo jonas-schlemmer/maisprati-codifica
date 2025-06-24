63a9595fd856eef84d58d27420a7fc0bf151d571
Jonas Schlemmer <jonas.schlemmer@gmail.com>
2025-06-24 11:59:17 -0300

    9th exercise completed

--- modulo-01/exercicios-02/09-conversaoFormatos.js
+++ modulo-01/exercicios-02/09-conversaoFormatos.js
@@ -1,4 +1,26 @@
 // 9. Escreva duas funções: 
 // paresParaObjeto(pares) recebe um array de pares [ [chave, valor], ... ] e retorna o objeto equivalente.
 // objetoParaPares(obj) faz o inverso, retornando um array de pares. 
- 
+
+function paresParaObjeto(pares) {
+    return Object.fromEntries(pares)
+}
+
+function objetoParaPares(obj) {
+    return Object.entries(obj)
+}
+
+const pares = [
+    ['nome', 'João'],
+    ['idade', 30],
+    ['cidade', 'São Paulo'],
+    ['profissao', 'Desenvolvedor']
+]
+
+console.log('Array de pares:', pares)
+
+const obj = paresParaObjeto(pares)
+console.log('Convertido para objeto:', obj)
+
+const paresDeNovo = objetoParaPares(obj)
+console.log('Objeto convertido de volta para pares:', paresDeNovo)
