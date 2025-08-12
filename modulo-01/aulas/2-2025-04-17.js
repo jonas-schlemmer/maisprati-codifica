f05abc07fbc6ca206c3ae831bd0113cbe78e02e1
jonas-schlemmer <jonas.schlemmer@gmail.com>
2025-08-12 20:40:34 -0300

    Finalizado aula 1

---
+++ modulo-01/aulas/2-2025-04-17.js
@@ -0,0 +1,19 @@
+console.log('A gíria "mané" é muito utilizada no RJ')
+
+let emocao = "feliz"
+console.log(`Jonas Schlemmer é ${emocao}`)
+
+console.log(typeof("Eu sou uma String!"))
+console.log(typeof(1.5))
+
+let name = 'Jonas'
+console.log(name)
+
+var anothername = "Schlemmer"
+console.log(anothername)
+
+const CITY = "Sapiranga"
+console.log(CITY)
+
+let test = null
+console.log(test)
--- modulo-01/aulas/z-basic-structure.js
+++ modulo-01/aulas/z-basic-structure.js
@@ -1,28 +1,3 @@
-// console.log("Hello World!")
-// console.log('A gíria "mané" é muito utilizada no RJ')
-
-// let emocao = "feliz"
-// console.log(`Jonas Schlemmer é ${emocao}`)
-
-// console.log(typeof("Eu sou uma String!"))
-// console.log(typeof(1.5))
-
-//////
-
-// let name = 'Jonas'
-// console.log(name)
-
-// let anothername = "Schlemmer"
-// console.log(anothername)
-
-// const CITY = "Sapiranga"
-// console.log(CITY)
-
-// let test = null
-// console.log(test)
-
-//////
-
 // let num1 = 9, num2 = 3
 // console.log(num1 + num2)
 // console.log(num1 - num2)
