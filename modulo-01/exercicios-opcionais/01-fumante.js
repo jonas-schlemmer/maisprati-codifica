56a30dfd089ee7dd6c469768a86af7d0d8232ba1
Jonas Schlemmer <jonas.schlemmer@gmail.com>
2025-08-20 17:37:00 -0300

    Teste

--- modulo-01/exercicios-opcionais/01-fumante.js
+++ modulo-01/exercicios-opcionais/01-fumante.js
@@ -1,1 +1,10 @@
 // 1. Escreva um programa para calcular a redução do tempo de vida de um fumante. Pergunte a quantidade de cigarros fumados por dias e quantos anos ele já fumou. Considere que um fumante perde 10 min de vida a cada cigarro. Calcule quantos dias de vida um fumante perderá e exiba o total em dias.
+const prompt = require('prompt-sync')()
+
+let name = prompt('Qual seu nome?')
+
+console.log(saudar())
+
+function saudar() {
+    return `Olá ${name}! Como vai?`
+}
