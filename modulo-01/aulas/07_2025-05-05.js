48713f3925befc38de17396a9145925f94f7635f
Jonas Schlemmer <jonas.schlemmer@gmail.com>
2025-09-07 23:02:50 -0300

    Aulas 7 e 8 concluídas

--- modulo-01/aulas/07_2025-05-05.js
+++ modulo-01/aulas/07_2025-05-05.js
@@ -1,27 +1,25 @@
-// let pares = []
 // Crie um array chamado 'numeros' contendo 10 valores numéricos à sua escolha.
 let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-let soma = numeros.reduce((acumulador, valor) => acumulador + valor, 0)
-let pares = numeros.filter((num => num % 2 === 0))
-console.table(soma)
-console.table(pares)
+
 // Use métodos de array para calcular:
 // A soma de todos os elementos.
-// let soma = 0
+let soma = numeros.reduce((acumulador, valor) => acumulador + valor, 0)
+console.table(soma)
 
-// for(let i = 0; i< numeros.length; i++) {
-//     soma += numeros[i]
-//     if (numeros[i] % 2 == 0) {
-//         pares.push(numeros[i])
-//     }
-// }
+// Gere um novo array chamado pares que contenha apenas os números pares de números.
+let pares = numeros.filter((num => num % 2 === 0))
+console.table(pares)
 
-// console.log(soma);
+for(let i = 0; i< numeros.length; i++) {
+    soma += numeros[i]
+    if (numeros[i] % 2 == 0) {
+        pares.push(numeros[i])
+    }
+}
 
 // A média dos elementos.
 console.log(soma / numeros.length)
 
-// Gere um novo array chamado pares que contenha apenas os números pares de números.
 // console.log(pares)
 
 // Imprima no console:
