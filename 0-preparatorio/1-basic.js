69025a5ce64c1379aab0d129f0568f45c8211be5
Jonas Schlemmer <jonas.schlemmer@gmail.com>
2026-03-15 16:08:38 -0300

    Aula 2 do preparatório concluída

---
+++ 0-preparatorio/1-basic.js
@@ -0,0 +1,169 @@
+// console.log("Jonas")
+// console.log('Schlemmer')
+// console.log(`Internacional`)
+
+// console.log(1.2)
+// console.log("4 é um número.")
+// console.log("Jonas", 1.0, 1000)
+
+// let nome = "Jonas"
+// console.log(nome)
+
+// var outroNome = "Schlemmer"
+// console.log(outroNome)
+
+// var outroNome = "Pedro"
+// console.log(outroNome)
+// nome2 = "João"
+// let valor
+
+// const NAME = "Jonas Schlemmer"
+
+// let test
+// console.log(test)
+
+// test = "valor"
+// console.log(test)
+
+// let meuNull = null
+// console.log(meuNull)
+
+// let myboolean = true;
+// console.log(myboolean)
+
+// myboolean = false;
+// console.log(myboolean)
+
+// let minhaString = "Isto é uma string"
+// let meuNumber = 15.30
+
+// console.log(typeof(myBoolean))
+
+// let qualquer = 10
+// console.log(typeof(qualquer))
+
+// qualquer = "Jaques"
+// console.log(typeof(qualquer))
+
+// let num1 = 20
+// let num2 = 2
+
+// console.log(num1 + num2)
+// console.log(num1 - num2)
+// console.log(num1 / num2)
+// console.log(num1 * num2)
+// console.log(num1 % num2)
+// console.log(10 + 10)
+// console.log(2 ** 2)
+
+// console.log("2" + 2)
+// console.log("O Jonas tem " + num1 + " anos de idade.")
+// console.log("O Jonas tem ", num1, " anos de idade.")
+// console.log(`O Jonas tem ${num1} anos de idade.`)
+
+// console.log((2+10)*2)
+
+// let num = "4"
+// let num2 = 10
+
+// console.log(num + num2)
+// let num3 = parseInt(num)
+// console.log(num3 + num2)
+
+// let contador = 1
+// contador = contador + 1
+// contador++
+// contador++
+// contador++
+
+// console.log(contador)
+
+// contador = contador -1
+// contador += 1
+// contador--
+
+// console.log(contador) // 1
+// console.log(contador++)// 1
+// console.log(contador) // 2
+// console.log(++contador)// 3
+// console.log(contador)// 3
+
+// contador += 1
+// contador -= 1
+// contador /= 10
+// contador *= 5
+// contador = contador * 5
+
+// console.log(contador)
+
+// console.log(10 < 5)
+// console.log(10 > 5)
+// console.log(10 >= 5)
+// console.log(10 <= 5)
+// console.log(10 == "10")
+// console.log(10 === "10")
+// console.log(10 != "10")
+// console.log(10 !== "10")
+
+// Ponto Flutuante
+// console.log(0.2 + 0.7)
+
+// console.log((10 > 5) && (10 > 2))
+// console.log((4 > 2) || (1 > 1))
+// console.log((10 > 2 ) && !(1 > 0))
+
+/* Criar 2 variáveis (Login e Senha) e printem na tela se um usuário está ou não autenticado. */
+
+// let login = 'jonas'
+// let senha = 12345
+
+// let autenticacao = login === 'jonas' && senha === 12345
+
+// console.log(autenticacao)
+
+// let nota = 10
+
+// if (nota > 6) {
+//     console.log("APROVADO!")
+// }
+
+// console.log("Programa finalizado.")
+
+// let nota = 4
+
+// if (nota >= 6) {
+//     console.log("APROVADO!")
+// } else {
+//     console.log("Reprovado!")
+// }
+
+// console.log("Programa finalizado.")
+
+// let nota = 2
+
+// if (nota >= 6) {
+//     console.log("APROVADO!")
+// } else if (nota >= 4) {
+//     console.log("Exame!")
+// } else {
+//     console.log("Reprovado!")
+// }
+
+// console.log("Programa finalizado.")
+
+/* Façam um programa que avalie se uma pessoa está apta a tirar a carta de motorista. */
+
+// let idade = 17
+
+// if (idade >= 18) {
+//     console.log("Você está apto a fazer a prova da CNH")
+// } else {
+//     console.log("Você ainda não está apto.")
+// }
+
+{/* <condicao> ? <verdadeiro> : <falso> */}
+// let idade = 20
+
+// let ternario = (idade >= 18) ? "Você está apto a fazer a prova da CNH" : "Você ainda não está apto."
+
+// console.log(ternario)
