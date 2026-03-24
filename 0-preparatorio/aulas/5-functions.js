89be222140753dd996589abd236e9bdef9e5ae2d
Jonas Schlemmer <jonas.schlemmer@gmail.com>
2026-03-24 19:07:52 -0300

    Aula 5 do preparatório concluída

---
+++ 0-preparatorio/aulas/5-functions.js
@@ -0,0 +1,22 @@
+// function incrementar(ref) {
+//     ref.valor++
+// }
+
+// let contador = {
+//     valor: 0
+// }
+
+// incrementar(contador)
+
+// console.log(contador.valor)
+
+// function fatorial(n) {
+//     if (n <= 1) {
+//         return n
+//     }
+
+//     return n * fatorial(n - 1)
+// }
+
+// console.log(fatorial(5))
+
