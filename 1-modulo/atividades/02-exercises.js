c0010b4a35f6cfd7da4d8527559badfe322e79c5
Jonas Schlemmer <jonas.schlemmer@gmail.com>
2026-05-29 15:32:21 -0300

    Finalizado exercícios 3 e 4 da lista 2 de exercícios

--- 1-modulo/atividades/02-exercises.js
+++ 1-modulo/atividades/02-exercises.js
@@ -15,7 +15,7 @@ function main() {
     console.clear()
 
     // Exibe o menu de opções
-    console.log(` 1 - Tabuada\n 2 - Dígitos\n 3 - xxx\n 4 - xxx\n 5 - xxx\n 6 - xxx\n 7 - xxx\n 8 - xxx\n 9 - xxx\n10 - xxx\n\n 0 - ENCERRAR PROGRAMA\n======================\n`)
+    console.log(` 1 - Tabuada\n 2 - Dígitos\n 3 - Fibonacci\n 4 - Senha\n 5 - xxx\n 6 - xxx\n 7 - xxx\n 8 - xxx\n 9 - xxx\n10 - xxx\n\n 0 - ENCERRAR PROGRAMA\n======================\n`)
     
     let option = Number(prompt(`Digite sua escolha: `))
     switch (option) {
@@ -114,4 +114,66 @@ function ex2() {
     console.log(`Total de Dígitos: ${total}`)
     
     menu()
+}
+
+// =========================
+// 3. Peça ao usuário quantos termos da sequência de Fibonacci deseja ver e exiba-os usando um laço for. Exemplo: 1, 1, 2, 3, 5, 8, 13...
+// =========================
+function ex3() {
+    
+    console.clear()
+
+    let number = Number(prompt(`Digite quantos termos da sequência de Fibonacci deseja ver: `))
+    let currentNumber = 1
+    let previousNumber = 0
+    let updatedNumber
+
+    if (!Number.isInteger(number) || number < 0) {
+        console.log(`\n===============================\n [ERRO] Digite somente números inteiros e positivos\n===============================`)
+        return setTimeout(() => {
+            console.clear()
+            ex3()
+        }, 3000)
+    }
+
+    for (let i = 1; i <= number; i++) {
+        console.log(previousNumber)
+        updatedNumber = currentNumber + previousNumber
+        previousNumber = currentNumber
+        currentNumber = updatedNumber
+    }
+    
+    menu()
+}
+
+// =========================
+// 4. Defina uma senha fixa no código. Peça ao usuário que a digite e, usando um laço do...while, permita no máximo 3 tentativas. Exiba se ele acertou ou se esgotou as tentativas.
+// =========================
+function ex4() {
+    
+    console.clear()
+
+    let password = 'Inter1'
+    let passwordInput
+    let remainingAttempts = 3
+    
+    do {
+        if (remainingAttempts === 0) {
+            console.clear()
+            console.log(`\nUsuário bloqueado!`)
+            return menu()
+        } else {
+            passwordInput = prompt(`Digite a senha: `)
+
+            if (password !== passwordInput) {
+                --remainingAttempts
+                console.clear()
+                console.log(`\nSenha inválida\nTentativas restantes: ${remainingAttempts}\n`)
+            } else {
+                console.clear()
+                console.log(`\nAcesso liberado!`)
+                return menu()
+            }
+        }
+    } while (password !== passwordInput)
 }
