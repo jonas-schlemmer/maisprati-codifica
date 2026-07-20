8dc5109acb09c6868e4a3d003e7c648e6d55123c
Jonas Schlemmer <jonas.schlemmer@gmail.com>
2026-07-20 11:29:09 -0300

    Adicionado conteúdo principal
    Adicionado ícone, seções no index, css e script do botão de inscrição.

---
+++ 2-modulo/atividades/4-5-site/scripts/script.js
@@ -0,0 +1,7 @@
+const btn_register = document.getElementById('btn-register')
+btn_register.addEventListener('click', () => {
+    document.getElementById('contact').scrollIntoView({
+        behavior: "smooth"
+    })
+})
+
