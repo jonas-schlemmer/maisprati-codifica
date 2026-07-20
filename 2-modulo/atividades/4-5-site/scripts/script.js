2ae908507cf9d1430b451aa213cd2ecb18d90625
Jonas Schlemmer <jonas.schlemmer@gmail.com>
2026-07-20 14:25:39 -0300

    v1.2
    Adicionado botão "voltar ao topo"

--- 2-modulo/atividades/4-5-site/scripts/script.js
+++ 2-modulo/atividades/4-5-site/scripts/script.js
@@ -57,3 +57,20 @@ cepInput.addEventListener('input', () => {
 
     cepInput.value = value
 })
+
+const btnTop = document.getElementById("btn-top")
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY > 300) {
+        btnTop.classList.add("show")
+    } else {
+        btnTop.classList.remove("show")
+    }
+})
+
+btnTop.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+})
