a7aa485d0ffb97e912db4c2775746d3bd18e412d
Jonas Schlemmer <jonas.schlemmer@gmail.com>
2026-09-05 19:01:24 -0300

    Aula 25 concluída

---
+++ 2-modulo/aulas/aula25/cadastro.js
@@ -0,0 +1,21 @@
+import { buscarCep } from "./services/viacep.js"
+
+// let form = document.querySelector('#form-cadastro')
+let campoCep = document.querySelector('#cep')
+
+campoCep.addEventListener('blur', async() => {
+    if (!campoCep.value) return
+
+    try {
+        let endereco = await buscarCep(campoCep.value)
+        document.querySelector('#logradouro').value = endereco.logradouro
+        document.querySelector('#bairro').value = endereco.bairro
+        document.querySelector('#cidade').value = endereco.localidade
+        document.querySelector('#uf').value = endereco.uf
+        
+        document.querySelector("#numero").focus();
+    } catch (error) {
+        console.log(error.message)
+    }
+
+})
