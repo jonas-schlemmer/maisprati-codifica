2ce6107ee38f39343f750e4bb5f26835a21ebfa8
Jonas Schlemmer <jonas.schlemmer@gmail.com>
2026-09-05 14:27:08 -0300

    Aula 23 concluída

---
+++ 2-modulo/aulas/aula23/js/components/site-header.js
@@ -0,0 +1,18 @@
+class SiteHeader extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML =
+        `<header class="cabecalho">
+            <div class="logo">
+                Clarim Diário
+            </div>
+            <nav class="navegacao">
+                <a href="#">Política</a>
+                <a href="#">Economia</a>
+                <a href="#">Esportes</a>
+                <a href="#">Homem-Aranha</a>
+            </nav>
+        </header>`
+    }
+}
+
+customElements.define('site-header', SiteHeader)
