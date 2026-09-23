b3900b29524387c4d9d8242b21f11e4b89693b0a
Jonas Schlemmer <jonas.schlemmer@gmail.com>
2026-09-22 22:07:29 -0300

    Aula 33 concluída

--- 3-modulo/clarim-diario/src/App.jsx
+++ 3-modulo/clarim-diario/src/App.jsx
@@ -1,10 +1,35 @@
 import Header from './components/Header/Header'
+import NewsCard from './components/NewsCard/NewsCard'
+import { noticias } from './data/noticias'
+import './App.css'
 
 function App() {
+  const [manchete, ...demais] = noticias
 
   return (
     <>
       <Header />
+
+      <main className='container'>
+        <section className='manchete'>
+          <NewsCard 
+            categoria = {manchete.categoria}
+            titulo = {manchete.titulo}
+            resumo = {manchete.resumo}  
+          />
+        </section>
+
+        <section className='grade'>
+          {demais.map((noticia) => (
+            <NewsCard
+              key = {noticia.id}
+              categoria = {noticia.categoria}
+              titulo = {noticia.titulo}
+              resumo = {noticia.resumo}
+            />
+          ))}
+        </section>
+      </main>
     </>
   )
 }
